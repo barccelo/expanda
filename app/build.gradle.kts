06c6a91f0c6b38a409b212c32bdd309b98241269
barccelo <160421933+barccelo@users.noreply.github.com>
2026-09-20 15:11:46 -0400

    chore: bump personal build for selection toolbar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "dev.barccelo.expanda"
         minSdk = 26
         targetSdk = 36
-        versionCode = 7
-        versionName = "0.3.2-personal.1"
+        versionCode = 8
+        versionName = "0.3.2-personal.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
